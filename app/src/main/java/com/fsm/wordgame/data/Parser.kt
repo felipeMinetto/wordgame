61c7b0bc32f4b053657c51597ccce0f6744a60f6
Felipe <felipe_minetto@bitbucket.org>
2019-08-17 21:48:52 -0300

    Change of parser library.

--- app/src/main/java/com/fsm/wordgame/data/Parser.kt
+++ app/src/main/java/com/fsm/wordgame/data/Parser.kt
@@ -1,19 +1,22 @@
 package com.fsm.wordgame.data
 
 import com.fsm.wordgame.model.TranslationPair
-import com.google.gson.GsonBuilder
+import com.squareup.moshi.Moshi
+import com.squareup.moshi.Types
 import io.reactivex.Single
 import io.reactivex.schedulers.Schedulers
 import java.io.InputStream
 
+
 object Parser {
-    private val gson = GsonBuilder().create()
 
     fun parseFromJsonIS(jsonIS: InputStream): Single<MutableList<TranslationPair>> {
+        val moshi = Moshi.Builder().build()
         return Single.fromCallable {
             val jsonText = String(jsonIS.readBytes())
-            val type = emptyList<TranslationPair>().toMutableList().javaClass
-            return@fromCallable gson.fromJson(jsonText, type)
+            val type = Types.newParameterizedType(MutableList::class.java, TranslationPair::class.java)
+            val adapter = moshi.adapter<MutableList<TranslationPair>>(type)
+            return@fromCallable adapter.fromJson(jsonText) ?: emptyList<TranslationPair>().toMutableList()
         }.subscribeOn(Schedulers.io())
     }
 }
